e95cb73332b603d28289f092c782af7bddb1f126
Joshua Shannon <retrodaredevil@gmail.com>
2019-06-28 15:59:20 -0600

    Relay activates when new line received

--- clap-activator/clap_activator.ino
+++ clap-activator/clap_activator.ino
@@ -1,13 +1,21 @@
 #include <Arduino.h>
 #include "User_Setup.h"
 
+const int RELAY_PIN = 7;
+
 void setup() {
-    pinMode(13, OUTPUT);
+    pinMode(RELAY_PIN, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
-    digitalWrite(13, HIGH);
-    delay(250);
-    digitalWrite(13, LOW);
-    delay(250);
+    while(Serial.available() > 0){
+        char c = Serial.read();
+        if(c == '\n'){
+            digitalWrite(RELAY_PIN, HIGH);
+            delay(700);
+            digitalWrite(RELAY_PIN, LOW);
+        }
+    }
+    delay(10);
 }
